764e743c2dadf0795eb16711f6cb76ffae57b864
RowanSe7en <bilal.rouane702@gmail.com>
2026-03-08 21:17:03 +0000

    c2r7p6vm

--- python05/ex2/nexus_pipeline.py
+++ python05/ex2/nexus_pipeline.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, List, Dict, Union, Optional, Protocol
+from typing import Any, List, Dict, Union, Protocol
 
 
 class ProcessingStage(Protocol):
